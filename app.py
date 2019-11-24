28058956963b070d1914833af79f640501139e0f
bj254 <bj254@cornell.edu>
2019-11-24 17:01:57 -0500

    Updated relationships and new methods

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import json
 from flask import Flask, request
-from db import db, Post, User
+from db import db, Post, User, Comment
 import datetime
-
+from sqlalchemy import desc
 app = Flask(__name__)
 db_filename = 'potd.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///%s' % db_filename
@@ -13,153 +13,128 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
-post_id_counter = 0
 
-comment_id_counter = 0
-
-posts = {
-}
-
-comments = {
-
-}
 @app.route('/')
 # GET all posts (time order)
 @app.route('/api/posts/', methods=['GET'])
 def get_all_posts():
-    res = {'success': True, 'data': list(posts.values())}
+    posts = Post.query.all()
+    res = {'success': True, 'data': [p.serialize() for p in posts]}
     return json.dumps(res), 200
 
 # GET all posts (upvote order)
-@app.route('/api/posts/upvote/order/', methods=['GET'])
+@app.route('/api/posts/upvote/', methods=['GET'])
 def get_all_posts_order():
-    sort_posts = sorted(posts.items(), key=lambda e: e[1]['upvotes'])
-    res = {'success': True, 'data': sort_posts}
+    posts = Post.query.order_by(desc(Post.upvotes)).all()
+    res = {'success': True, 'data': [p.serialize() for p in posts]}
     return json.dumps(res), 200
 
 # POST(create) a post
 @app.route('/api/posts/', methods=['POST'])
 def make_a_post():
-    global post_id_counter
     post_body = json.loads(request.data)
-    title = post_body['title']
-    body = post_body['body']
-    username = post_body['username']
-    newPost = {
-        'id': post_id_counter,
-        'upvotes': 1,
-        'title': title,
-        'body': body,
-        'username': username,
-        'timestamp': format(datetime.datetime.now())
-    }
-    posts[post_id_counter] = newPost
-    post_id_counter += 1
-    res = {'success': True, 'data': newPost}
-    return json.dumps(res), 201
+    body_post = post_body.get('body')
+    username = post_body.get('username')
+    user = User.query.filter_by(username=username).first()
+    user_id = user.id
+    newPost = Post(
+        upvotes=0,
+        body_post=body_post,
+        time_stamp=format(datetime.datetime.now()),
+        user_id=user_id
+    )
+    db.session.add(newPost)
+    db.session.commit()
+    return json.dumps({'success': True, 'data': newPost.serialize()}), 201
 
 # GET a specific post
 @app.route('/api/post/<int:post_id>/', methods=['GET'])
 def get_a_post(post_id):
-    post = posts.get(post_id, None)
+    post = Post.query.filter_by(id=post_id).first()
     if not post:
         return json.dumps({'success': False, 'error': 'Post not found'}), 404
-    return json.dumps({'success': True, 'data': post}), 200
+    return json.dumps({'success': True, 'data': post.serialize()}), 200
 
 # GET top post
 @app.route('/api/post/top/', methods=['GET'])
 def get_top_post():
-    global posts
-    max_upvote = 0
-    max_post = []
-    for i in range(len(posts)):
-        post = posts[i]
-        upvote = post['upvotes']
-        if upvote > max_upvote:
-            max_upvote = upvote
-            max_post = post
-    return json.dumps({'success': True, 'data': max_post}), 200
+    post = Post.query.order_by(desc(Post.upvotes)).first()
+    res = {'success': True, 'data': post.serialize()}
+    return json.dumps(res), 200
 
 # DELETE a specific post
 @app.route('/api/post/<int:post_id>/', methods=['DELETE'])
 def delete_a_post(post_id):
-    post = posts.get(post_id, None)
+    post = Post.query.filter_by(id=post_id).first()
     if not post:
         return json.dumps({'success': False, 'error': 'Post not found'}), 404
-    del posts[post_id]
-    return json.dumps({'success': True, 'data': post}), 200
-
-# GET comments for a specific post
-@app.route('/api/post/<int:post_id>/comments/', methods=['GET'])
-def get_comments(post_id):
-    post = posts.get(post_id, None)
-    if not post:
-        return json.dumps({'success': False, 'error': 'Post not found'}), 404
-    lst = []
-    for c_id in comments.keys():
-        if comments[c_id]['postID'] == post_id:
-            comment = {
-                'id': comments[c_id]['id'],
-                'upvotes': comments[c_id]['upvotes'],
-                'text': comments[c_id]['text'],
-                'username': comments[c_id]['username'],
-            }
-            lst.append(comment)
-    return json.dumps({'success': True, 'data': lst}), 200
-
-# POST a comment for a specific post
-@app.route('/api/post/<int:post_id>/comment/', methods=['POST'])
-def post_a_comment(post_id):
-    global comment_id_counter
-    post_body = json.loads(request.data)
-    text = post_body['text']
-    username = post_body['username']
-    newComment = {
-        'id': comment_id_counter,
-        'upvotes': 1,
-        'text': text,
-        'username': username,
-        'postID': post_id,
-    }
-    comment = {
-        'id': newComment['id'],
-        'upvotes': newComment['upvotes'],
-        'text': newComment['text'],
-        'username': newComment['username'],
-    }
-    comments[comment_id_counter] = newComment
-    comment_id_counter += 1
-    res = {'success': True, 'data': comment}
-    return json.dumps(res), 201
-
-# POST an edited comment
-@app.route('/api/post/<int:post_id>/comment/<int:comment_id>/', methods=['POST'])
-def edit_a_comment(post_id, comment_id):
-    post = posts.get(post_id, None)
-    if not post:
-        return json.dumps({'success': False, 'error': 'Post not found'}), 404
-    oldComment = comments.get(comment_id, None)
-    if not oldCcomment:
-        return json.dumps({'success': False, 'error': 'Comment not found'}), 404
-    post_body = json.loads(request.data)
-    oldComment['text'] = post_body['text']
-    comment = {
-        'id': oldComment['id'],
-        'upvotes': oldComment['upvotes'],
-        'text': oldComment['text'],
-        'username': oldComment['username'],
-    }
-    return json.dumps({'success': True, 'data': comment}), 200
+    db.session.delete(post)
+    db.session.commit()
+    return json.dumps({'success': True, 'data': post.serialize()}), 200
 
 # Upvote a post
 @app.route('/api/post/<int:post_id>/upvote/', methods=['POST'])
 def upvote_post(post_id):
-    global posts
-    post = posts.get(post_id, None)
+    post = Post.query.filter_by(id=post_id).first()
     if not post:
-        return json.dumps({'success': False, 'error': 'Post Not Found'}), 404
-    post['upvotes'] = post['upvotes'] + 1
-    return json.dumps({'success': True, 'data': post}), 200
+        return json.dumps({'success': False, 'error': 'Post not found'}), 404
+    post.upvotes += 1
+    db.session.commit()
+    return json.dumps({'success': True, 'data': post.serialize()}), 200
+
+# GET users
+@app.route('/api/users/', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    res = {'success': True, 'data': [u.serialize() for u in users]}
+    return json.dumps(res), 200
+
+# POST user
+@app.route('/api/users/', methods=['POST'])
+def make_user():
+    post_body = json.loads(request.data)
+    username = post_body.get('username')
+    newUser = User(
+        username=username
+    )
+    db.session.add(newUser)
+    db.session.commit()
+    return json.dumps({'success': True, 'data': newUser.serialize()}), 201
+
+# DELETE user
+@app.route('/api/user/<int:user_id>/', methods=['DELETE'])
+def delete_user(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    if not user:
+        return json.dumps({'success': False, 'error': 'User not found'}), 404
+    db.session.delete(user)
+    db.session.commit()
+    return json.dumps({'success': True, 'data': user.serialize()}), 200
+
+# GET comments
+@app.route('/api/comments/', methods=['GET'])
+def get_comments():
+    comments = Comment.query.all()
+    res = {'success': True, 'data': [c.serialize() for c in comments]}
+    return json.dumps(res), 200
+
+# POST comment
+@app.route('/api/comments/<int:post_id>/', methods=['POST'])
+def post_comment(post_id):
+    post_body = json.loads(request.data)
+    body_comment = post_body.get('body')
+    username = post_body.get('username')
+    user = User.query.filter_by(username=username).first()
+    user_id = user.id
+    newComment = Comment(
+        body_comment=body_comment,
+        time_stamp=format(datetime.datetime.now()),
+        user_id=user_id,
+        post_id=post_id
+    )
+    db.session.add(newComment)
+    db.session.commit()
+    return json.dumps({'success': True, 'data': newComment.serialize()}), 201
 
 
 """
--- db.py
+++ db.py
@@ -2,44 +2,41 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 # Your classes here
-user_post_table = db.Table('user_post', db.Model.metadata,
-                           db.Column('users_id', db.Integer,
-                                     db.ForeignKey('users.id')),
-                           db.Column('posts_id', db.Integer,
-                                     db.ForeignKey('posts.id'))
-                           )
 
 
 class Post(db.Model):
-    __tablename__ = 'posts'
+    __tablename__ = 'posts_table'
     id = db.Column(db.Integer, primary_key=True)
     upvotes = db.Column(db.Integer, nullable=False)
     body_post = db.Column(db.String, nullable=False)
     time_stamp = db.Column(db.String, nullable=False)
-    users = db.relationship(
-        'User', secondary=user_post_table, back_populates='posts')
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        'users_table.id'), nullable=False)
+    comments = db.relationship('Comment', cascade='delete')
 
     def __init__(self, **kwargs):
         self.upvotes = kwargs.get('upvotes', '')
-        self.body_post = kwargs.get('body', '')
-        self.time_stamp = kwargs.get('time stamp', '')
-        self.users = []
+        self.body_post = kwargs.get('body_post', '')
+        self.time_stamp = kwargs.get('time_stamp', '')
+        self.user_id = kwargs.get('user_id', '')
 
     def serialize(self):
         return {
             'id': self.id,
             'upvotes': self.upvotes,
             'body_post': self.body_post,
-            'users': [u.serialize() for u in self.users]
+            'user_id': self.user_id,
+            'time_stamp': self.time_stamp,
+            'comments': [c.serialize() for c in self.comments]
         }
 
 
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = 'users_table'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, nullable=False)
-    posts = db.relationship(
-        'Posts', secondary=user_post_table, back_populates='users')
+    posts = db.relationship('Post', cascade='delete')
+    comments = db.relationship('Comment', cascade='delete')
 
     def __init__(self, **kwargs):
         self.username = kwargs.get('username', '')
@@ -48,5 +45,33 @@ class User(db.Model):
     def serialize(self):
         return{
             'id': self.id,
-            'username': self.username
+            'username': self.username,
+            'posts': [p.serialize() for p in self.posts],
+            'comments': [c.serialize() for c in self.comments]
+        }
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments_table'
+    id = db.Column(db.Integer, primary_key=True)
+    body_comment = db.Column(db.String, nullable=False)
+    time_stamp = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        'users_table.id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey(
+        'posts_table.id'), nullable=False)
+
+    def __init__(self, **kwargs):
+        self.body_comment = kwargs.get('body_comment', '')
+        self.time_stamp = kwargs.get('time_stamp', '')
+        self.user_id = kwargs.get('user_id', '')
+        self.post_id = kwargs.get('post_id', '')
+
+    def serialize(self):
+        return{
+            'id': self.id,
+            'body_comment': self.body_comment,
+            'time_stamp': self.time_stamp,
+            'user_id': self.user_id,
+            'post_id': self.post_id
         }
